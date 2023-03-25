218fc187ab2914d4d41c1573a8257553dd303143
jtuu <joona.tuukkanen@gmail.com>
2023-03-25 02:03:08 +0200

    Added option to only export selected objects

--- rel_addon/__init__.py
+++ rel_addon/__init__.py
@@ -1,8 +1,8 @@
 import os
 import bpy
 from bpy_extras.io_utils import ExportHelper
-from bpy.props import StringProperty
-from bpy.types import Operator, Object
+from bpy.props import StringProperty, EnumProperty
+from bpy.types import Operator, Object, Panel
 from . import r_rel, n_rel, c_rel
 
 
@@ -38,19 +38,61 @@ class ExportRel(Operator, ExportHelper):
     filename_ext = ".rel"
 
     filter_glob: StringProperty(
-        # Suppress flake8 false errors
-        default="*.rel", # noqa: F722
-        options={"HIDDEN"}, # noqa: F821
+        default="*.rel",
+        options={"HIDDEN"},
         maxlen=255,  # Max internal buffer length, longer would be clamped.
+    )
+
+    export_strategy: EnumProperty(
+        name="Export",
+        options={"ENUM_FLAG"},
+        items=[
+            ("EXPORT_BY_TAGS", "By tags", "Export all objects in the scene based on their tags", 1),
+            ("EXPORT_SELECTED", "Selected only", "Only export selected objects as chosen format, ignoring their tags", 2)
+        ],
+        default={"EXPORT_BY_TAGS"}
+    )
+
+    export_as_format: EnumProperty(
+        name="As format",
+        items=[
+            ("EXPORT_AS_NREL", "n.rel", "", 1),
+            ("EXPORT_AS_CREL", "c.rel", "", 2),
+            ("EXPORT_AS_RREL", "r.rel", "", 3),
+            ("EXPORT_AS_NONE", "", "", 4)
+        ],
+        default="EXPORT_AS_NONE"
     )
 
     filepath: StringProperty(subtype="FILE_PATH") # noqa: F821
 
-    def cancel_with_error(self, ex: Exception()):
+    def cancel_with_error(self, ex: Exception):
         self.report({"ERROR"}, str(ex))
         return {"CANCELLED"}
+    
+    def cancel_with_warning(self, msg: str):
+        self.report({"WARNING"}, msg)
+        return {"CANCELLED"}
 
-    def execute(self, context):
+    def export_selected(self):
+        objs = bpy.context.selected_objects
+        if len(objs) < 1:
+            objs = bpy.context.collection.objects
+        if len(objs) < 1:
+            return self.cancel_with_warning("REL export error: No objects selected")
+        format_info = {
+            "EXPORT_AS_NREL": ("n", n_rel.write),
+            "EXPORT_AS_CREL": ("c", c_rel.write),
+            "EXPORT_AS_RREL": ("r", r_rel.write)
+        }
+        if self.export_as_format not in format_info:
+            return self.cancel_with_warning("REL export error: Invalid export format")
+        (filename_suffix, writer) = format_info[self.export_as_format]
+        (noext, ext) = os.path.splitext(self.filepath)
+        writer(noext + filename_suffix + ext, objs)
+        return {"FINISHED"}
+    
+    def export_all(self):
         try:
             minimap_objs = filter_objects_by_props(["rrel"])
             render_objs = filter_objects_by_props(["nrel"])
@@ -66,6 +108,45 @@ class ExportRel(Operator, ExportHelper):
             c_rel.write(noext + "c" + ext, collision_objs)
         return {'FINISHED'}
 
+    def execute(self, context):
+        if "EXPORT_SELECTED" in self.export_strategy:
+            return self.export_selected()
+        elif "EXPORT_BY_TAGS" in self.export_strategy:
+            return self.export_all()
+        return self.cancel_with_warning("REL export error: Invalid export settings")
+    
+    def draw(self, context):
+        pass
+
+
+class CUSTOM_PT_export_settings(Panel):
+    bl_space_type = 'FILE_BROWSER'
+    bl_region_type = 'TOOL_PROPS'
+    bl_label = "Settings Panel"
+    bl_options = {'HIDE_HEADER'}
+
+    @classmethod
+    def poll(cls, context):
+        sfile = context.space_data
+        operator = sfile.active_operator
+        return operator.bl_idname == "EXPORT_SCENE_OT_rel"
+
+    def draw(self, context):
+        layout = self.layout
+        layout.use_property_split = True
+        layout.use_property_decorate = False  # No animation.
+
+        sfile = context.space_data
+        operator = sfile.active_operator
+
+        box = layout.box()
+        box.row(align=True).prop(operator, "export_strategy")
+        export_as_format_row = box.row(align=True)
+        export_as_format_row.prop(operator, "export_as_format")
+        export_as_format_row.enabled = "EXPORT_SELECTED" in operator.export_strategy
+        if not export_as_format_row.enabled:
+            operator.export_as_format = "EXPORT_AS_NONE"
+
 
 def menu_func_export(self, context):
     self.layout.operator(ExportRel.bl_idname, text=bl_info["name"])
@@ -74,11 +155,13 @@ def menu_func_export(self, context):
 # Register and add to the "file selector" menu
 def register():
     bpy.utils.register_class(ExportRel)
+    bpy.utils.register_class(CUSTOM_PT_export_settings)
     bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
 
 
 def unregister():
     bpy.utils.unregister_class(ExportRel)
+    bpy.utils.unregister_class(CUSTOM_PT_export_settings)
     bpy.types.TOPBAR_MT_file_export.remove(menu_func_export)
 
 
